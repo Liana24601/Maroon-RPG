e0f694ef1583dac5cd681e7703a41a56cbb39be9
Liana H. <lianadothallik@gmail.com>
2025-05-29 22:01:56 -0400

    Update planning.js

--- planning.js
+++ planning.js
@@ -1,1 +1,26 @@
+/*
 
+this file is gonna be used for planning out the whole story
+game name: red stains and gold eyes
+
+basic story bullet points:
+- Thespian Robbie with gold eyeliner, like how cinna from the hunger games wore gold eyeliner 
+- How they meet: hang out after a party. In Robbie’s apartment. Robbie spills wine on the main character's burgundy t-shirt. They both collapse in giggles
+- Robbie: “How'd we end up on the floor anyway?"      Main character: “Your roommate's cheap-ass screw-top rosé, that's how” (lyric from the song)
+- More cute Robbie scenes showing his friendliness and initial charm ( shoeless dancing in New York, putting bandages on her collarbone cut, long calls on the landline “rust”)
+- MC means main character. Maroon becomes MCs fav color
+- MC falls cause Robbie is charming, dramatic, sweet, and lights up a room
+- Robbie’s a theater kid, born for the stage. It’s his greatest dream. MC is a comp sci major, just for the money “you hate to code, but you need it.”
+- A huge reason MC falls is because of his theater stuff. Robbie is so passionate about it. He’s convinced he’ll die if he doesn’t make it to broadway. He’s a true New York dreamer, believing the city is one that gives birth to stars
+- MC envies this , unlike Robbie she’s introverted and boring. She picked comp sci not because she loves it, but because it’s a reliable career.
+- Shows the growing problems between Robbie and MC
+- His cute theater things are getting annoying. He makes every molehill a mountain. He doesn’t pitch in money at all. He’s obsessed with the unachievable. He uses acting and dramatics to avoid hard conversations, He thinks he’ll be able to reach broadway when so many others failed. He goes to auditions and gets nothing for it. MC starts to hate him even more. (might cut this part)
+- Then the apartment floods. It’ll take funds to fix it. MC takes funds from Robbie’s savings for an art college.
+- When Robbie finds out he throws a fit. He cries he curses he recites angry Shakespeare lines like he’ll die. The last time he was this sad was when he had bought her carnations instead of roses by accident. MC wishes he’d shut up. She regrets ever meeting him. She voices everything she loathes about him, his her envy turned into pure hatred. They storm out into the apartment stairs they’re yelling. She pushes him down the stairs.
+- The neighbor comes out and she lies. She says robbie fell, and begs her to call an ambulance. The blood that spills from the crack on Robbie’s head is so beautifully maroon somehow. He looked “Hollow eyed” MC thinks Hell, my acting is super. Maybe i was the one meant for broadway. 
+- MC sees maroon in everything now. In the ambulance. In the siren. in the sunset that seems to taunt her. Robbie, all red blood and smudged eyeliner, haunts her like a Shakespeare ghost. He whispers snide comments at her back day by day. Sometimes in iambic pentameter.
+- end: MC  has become a terrible person. Sees maroon in everything. In Rockefeller center, sees a girl near the ice skating rink wearing a maroon shirt, like the one MC wore. In a fit of rage, and blinded by guilt, the MC follows her into an alley and st*bs with a knife, ghost Robbie convinces her to. Red everywhere.
+- The police find MC sitting in a pool of blood, laughing hysterically, maroon everywhere.
+
+
+*/
