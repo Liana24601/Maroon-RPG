e491392febcbc8dc5e7f029ac2d82e4bd1b84071
Liana H. <lianadothallik@gmail.com>
2025-06-21 17:04:18 -0400

    Update planning.js

--- planning.js
+++ planning.js
@@ -2,18 +2,17 @@
 
 this file is gonna be used for planning out the whole story
 game name: red stains and gold eyes
-(sorry if the commit timings are kinda weird, i got so invested writing this that i forgot to commit sometimes mb)
 
 basic story bullet points:
 - Thespian Robbie with gold eyeliner, like how cinna from the hunger games wore gold eyeliner 
 - How they meet: hang out after a party. In Robbie’s apartment. Robbie spills wine on the main character's burgundy t-shirt. They both collapse in giggles
-- Robbie: “How'd we end up on the floor anyway?"      Main character: “Your roommate's cheap-ass screw-top rosé, that's how” (lyric from the song)
+- Robbie: “How'd we end up on the floor anyway?"  Main character: “Your roommate's cheap-ass screw-top rosé, that's how” (lyric from the song)
 - More cute Robbie scenes showing his friendliness and initial charm ( shoeless dancing in New York, putting bandages on her collarbone cut, long calls on the landline “rust”)
 - MC means main character. Maroon becomes MCs fav color
 - MC falls cause Robbie is charming, dramatic, sweet, and lights up a room
 - Robbie’s a theater kid, born for the stage. It’s his greatest dream. MC is a comp sci major, just for the money “you hate to code, but you need it.”
 - A huge reason MC falls is because of his theater stuff. Robbie is so passionate about it. He’s convinced he’ll die if he doesn’t make it to broadway. He’s a true New York dreamer, believing the city is one that gives birth to stars
-- MC envies this , unlike Robbie she’s introverted and boring. She picked comp sci not because she loves it, but because it’s a reliable career.
+- MC envies this, unlike Robbie she’s introverted and boring. She picked comp sci not because she loves it, but because it’s a reliable career.
 - Shows the growing problems between Robbie and MC
 - His cute theater things are getting annoying. He makes every molehill a mountain. He doesn’t pitch in money at all. He’s obsessed with the unachievable. He uses acting and dramatics to avoid hard conversations, He thinks he’ll be able to reach broadway when so many others failed. He goes to auditions and gets nothing for it. MC starts to hate him even more. (might cut this part)
 - Then the apartment floods. It’ll take funds to fix it. MC takes funds from Robbie’s savings for an art college.
@@ -22,5 +21,141 @@ basic story bullet points:
 - MC sees maroon in everything now. In the ambulance. In the siren. in the sunset that seems to taunt her. Robbie, all red blood and smudged eyeliner, haunts her like a Shakespeare ghost. He whispers snide comments at her back day by day. Sometimes in iambic pentameter.
 - end: MC  has become a terrible person. Sees maroon in everything. In Rockefeller center, sees a girl near the ice skating rink wearing a maroon shirt, like the one MC wore. In a fit of rage, and blinded by guilt, the MC follows her into an alley and st*bs with a knife, ghost Robbie convinces her to. Red everywhere.
 - The police find MC sitting in a pool of blood, laughing hysterically, maroon everywhere.
+
+plot of the old maroon game:
+- they hang out after the play, the wine spill
+- cute falling in love scenes, dancing in new york, hour long calls
+- robbie gets annoying, mc falls out of love.
+- apartment floods, MC steals Robbie's funds for theatre school
+- robbie throws a fit, argument
+- push. robbies dead.
+- MC goes mad, kills random girl
+
+Rewriting Maroon:
+- player decisions influence story more!!
+- in depth characters and game sequences (Ex. the fund puzzle but more complex)
+- VISUALS!!
+- different endings (2)
+- rewrite story sigh
+
+Basic (new) plot brainstorming:
+- You wake up in a shadowy void. You are met by the Grim Reaper
+- The Reaper tells u the sitch. He explains how u killed robbie and the random girl but doesnt tell specifics
+- He says he is angry cause u messed with his work. u took two lives, when he needed one
+- since he liked robbie he is even more angry with you
+- he gives u a chance to make it up. Go back in time and fix everything
+- but if u dont, he'll take ur life
+- the player has to live through the romance and the fall again. but this time, find a way to fix everything, whether thats stopping the apartment from flooding, finding an alternate source of money to fix it, etc
+
+First scene writing brainstorming
+- Deep voice: "It's not often I give people second chances."
+- Deep voice: "I'm an impatient person, I'm told. Always rushing too fast before they're ready."
+- Deep voice: "Always showing up at the wrong times."
+- Deep voice: "But you, Scarlett?"
+- Deep voice: *Chuckle* "You're even worse than I am."
+- You (Scarlett): "who are you?"
+- Reaper: *EMERGES FROM SHADOWS*
+- Reaper: Who am I? *laughs* 
+- Reaper: I assure you, one day you will know who I am. I am all you are, all you know, and all you will be. It was I at the beginning and I who will remain.
+- You (Scarlett): "what do u want from me?"
+- Reaper: *The air grows heavy, you sense he's angry* You did me wrong, Scarlett. 
+- Reaper: I do not enjoy dealing with you humans, but I grew fond of both Robert and the girl. Kind souls, even if they were not perfect at times. Their eyes held dreams we may never know.
+- Reaper: So...when you killed him, I was angered. But when you took the life of that innocent girl due to your anger-driven insanity? I grew furious. 
+- Reaper: So I present to you this:
+- Reaper: I was in need of a soul that night. And when there is a debt, I must collect.
+- Reaper: I shall let you choose this time.
+- Reaper: You stole two souls last night. I only needed one. 
+- Reaper: You are to rewrite the story. You must make a choice. Save Robbie from yourself. Or save that girl's life. 
+- Reaper: If you succeed to save one of them, I will pursue my search elsewhere. Fail, and I will take the life of the one you hold most dear:
+- Reaper: Yourself
+- Reaper: Good luck, little human. But remember...
+- Reaper: It always comes back to maroon.
+
+You skim through your memories of the plot. You see the whole story for yourself.
+You choose who to save, the girl or robbie, or both?
+
+Girl Route:
+- It's too late for Robbie, but maybe not for her?
+- the game becomes a race to find her before time runs out.
+- you start two weeks before the incident, on the day u and robbie first meet.
+- you race around new york city, trying to look for her. You don't know anything but her face and her fateful maroon sweater.
+- You choose to use your memory of her face as your clue
+- Her face: you hire a sketch artist for a drawing of her. then you need to find a hacker who can put her face in an aligorithim.
+- You choose where to find the hacker. online from the dark shed the reaper placed u in or in person in shady underground markets where the hackers are found.
+- (you check in on robbie at many points of the game, u see your past self with him)
+- online: the "hacker" steals ur info. ur set back, with only a week until she dies. you give up and paste flyers of her face over the city. underground: you meet a shady but chill guy who agrees to help you.
+- flyers: she finds ur flyer, and is shocked. she calls you. chill guy: he finds her face in central park. you go.
+- call: you try to explain the situation to her. you dont wanna scare her. if u say "dont go to rockefeller" or "dont wear your maroon sweater on wednesday" before you quickly hang up hoping she'll listen. central park: you are ominous, she is confused. you speak to her from behind a tree, so she doesnt see ur face. all you see is her blonde hair. you have choose between saying "meet me at this cafe tomorrow, ill explain everything" or "dont wear your maroon sweater on wednesday" before you leave mysteriously.
+
+central park "maroon sweater":
+- you wake up in the morning. you have a text from the hacker. you have to choose to open the text or not.
+- if u open the text: you find the hacker saying "found ur girl at Lady Liberty today :)" with a photo of her from security cameras. your stomach drops. you realize your memory failed when u told the skecth artist. you said her eyes were blue, but they were actually brown.
+- if u dont open the text: you make a cup of coffee lazily. the hacker calls you repeatedly until u pick up. you recieve the same info but over call.
+- you realize that you have found the wrong girl. you panick until you realize you have to do something, maybe you can still find her. you tear off your pajamas, throw on a jacket and some decent clothes, grab ur keys, and run to the door
+- but as you reach rockefeller, there is a a crowd gathered around something. you see officers and ambulences. police tape. you ask a guy what happened. he tells u your worse fears while shaking his head: "some crazy lady just murdered this girl for no reason"
+- you push through the crowd to find the crime scene lined by police tape. officers stay circled around, shouting orders into walkie talkies.
+- but you can still see as your doppleganger cackles hysterically, drenched in blood, with a girl at her feet.
+- you watch in horror until your vision becomes dizzy and you find your self in a void of darkness
+- the reaper has come. you fear the worst. you have failed ur task. you haven't saved robbie or the girl. you have run out of time. your heartbeat quickens as you wait for the axe to fall
+- but the reaper is oddly calm and civil when he talks to you. he makes small talk about the weather "the sunset is betaiful today, vibrant and maroon.". you are shocked until he tells you:
+- "In all honesty, I really don't know what to do with you, little human. I admire your effort and dedication, one fo my favorite traits. However, you have ultimately failed your tasks. Thus, I am unsure whether the killing blow is rightfully deserved or not. But rather than make this perilous decsion myself, I will leave your fate in the hands of my good friends..."
+- "Luck." *A golden goddess appears, with a golden glow*
+- Luck: *happily* Hello there!
+- You: Uh...hi.
+- "And Wit." *a silver goddess with an Athena vibe appears. she has a spear*
+- You: Um, nice to meet you?
+- Wit: *nods* Pleasure.
+- "Pass their trials. Step carefully. Choose wisely. If all works well, you may see the sunset once again."
+- "But if you fail..."
+- "Not even I can help you now."
+- Luck: "Let's begin. I have four cards that will decide your fate. Ace of Spades. Jack of Clubs. Queen of Hearts. And the King of Diamonds. 2 will help. 2 will hurt. But don't worry, deary, your future is not decided until you complete the entire challenge. Choose 1."
+- Wit: "Scarlett. Be completely honest with me. Did you do this to save the girl (lies, compassion) or to save yourself (selfish, honesty)?" options: "save the girl" or "myself. im sorry"
+- they speak before turnign to you
+- Ace of Spades + "save the girl" = Wit: Luck and I have decided. You luck is spoiled and your intelligence non-existent. You picked the wrong card. And you lied to me in order to save yourself. 
+    - I'll have you know, child. I don't hace the patience for lies.
+    - An unknown force knocks you down. Wit points her spear at you, the silver tip an inch away from your heart.
+    - She looks at the reaper. "Waiting for your command, Reaper."
+    - You look at the reaper. You choose between saying "Please! I'll try again." or "Help me!"
+    - Both choices are useless. The reaper nods. "Proceed."
+    - Wit lifts her spear and then plunges it.
+- Queen of Hearts + "save the girl" = Wit: Luck and I have decided. Luck: You have rotten luck. It's a shame, dear. Wit: And your intelligence non-existent. You picked the wrong card. And you lied to me in order to save yourself. 
+    - I'll have you know, child. I don't hace the patience for lies.
+    - An unknown force knocks you down. Wit points her spear at you, the silver tip an inch away from your heart.
+    - She looks at the reaper. "Waiting for your command, Reaper."
+    - You look at the reaper. You choose between saying "Please! I'll try again." or "Help me!"
+    - Both choices are useless. The reaper nods. "Proceed."
+    - Wit lifts her spear and then plunges it.
+- Jack of Clubs + "myself. im sorry":
+- King of Diamonds + "myself. im sorry": Luck: *smiling* Wit and I have decided. You have an eye for the right cards. Your luck is pretty favorable!
+    - Wit: And I found your answers satisfactory. Honesty is an admirable trait.
+    - You: "Wait, does this mean..." or "What exactly are you saying?"
+    - Luck: *grins* YOU PASSED!!
+    - Wit: You have succeeded in completing our trial, and have proven yourself worthy.
+    - Luck: We wish you the best of luck in your future endeavors!
+    - Wit: Farewell, child. Life is a gift. Use it well.
+    - They disappear. You and the reaper are alone.
+    - Reaper: *chuckles* Quite a handful, aren't they?
+    - You: say "yeah, they were something lol" or "just nod. you dont want to risk it"
+    - Reaper: Humans, I find, are complex creatures. They spend years worrying about the wrong things only to meet me and realize that they have wasted their time.
+    - That they have let hatred and fear and greed consume them when they should have been enjoying their time here, which is shorter than one believes it to be.
+    - Not many get the chance to redo things, little human. You are a rare kind.
+    - So I ask that use this chance little human. Do not dwell on your past nor the things that are not in your grasp.
+    - I request that you spend your time wisely, fulfilling your own dreams.
+    - *Chuckles* As Wit said...
+    - Life is a gift *You wake up in your bed*
+    - Use it well. *You look at the sunset*
+
+
+call:
+- but u feel nervous before you go to bed, knowing it might happen tomorow. you decide to play a game of fate. the player can pick yes or no to the game. you have 10 pieces of paper. in five of them you write "leave it alone.", in the other 5, you write "see her tomorrow."
+- if u did not play the game and picked rockefeller she died. shes a rebellious person who wont listen to what she thinks is a prank call. the grim reaper takes u, as u failed to save robbie and her.
+- if u did not play the game and picked sweater she is saved. she is shocked that you know she has an exact red sweater and 
+- you pick a random pice. if u played the game, and picked see her tomorrow, you look across the rink to see her. she meets ur gaze and realized you were the one she called. as she gazes at u across the mall, your past self runs to her and stabs her with a knife from her purse. you failed.
+
+
+Robbie Route:
+- getting the funds to stop the apartment from blooding
+- if u succeed, you leave the money on the counter for your past self to find. 
+- if not, Robbie dies and the grim reaper takes you.
 
 */
